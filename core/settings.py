e3e82271b4007fceeea5089d03c69784a9eaa270
admin <admin@localhost>
2020-07-02 23:58:04 +0800

    处理笔记本上的路径

--- core/settings.py
+++ core/settings.py
@@ -2,7 +2,7 @@ import os as _os
 import socket
 
 IS_LAPTOP = False
-if socket.gethostname() == 'HAOYU-PAH-LAPTOP':
+if socket.gethostname() == 'HAOYU-PAN-LAPTOP':
     IS_LAPTOP = True
 
 FIGURE_PATH = r'D:\OneDrive - 同济大学\4. 非专业课\5. 偏微分方程数值解\试验作业2\插图'
